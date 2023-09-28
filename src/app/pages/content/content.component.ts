b890467f48238e4a46b09acf752d1d87d5880af1
Byatriz Moura <mourabyatriz@gmail.com>
2023-09-28 07:48:36 -0300

    dynamic part

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -6,15 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  @Input ()
   photoCover:string = ""
-  @Input ()
   contentTitle:string = ""
-  @Input ()
   contentDescription:string = ""
-  constructor() { }
+  private id:string | null = "0"
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
   }
 
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
 }
