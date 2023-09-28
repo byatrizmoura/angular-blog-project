7c8cf70a3dce393d7ece34224081ec060e4be3a7
Byatriz Moura <mourabyatriz@gmail.com>
2023-09-27 21:23:59 -0300

    pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photo":"aaa",
+    "title": "Lorem ipsum dolor sit amet.",
+    "description": "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet."
+  },
+  {
+    "id":2,
+    "photo":"aaa",
+    "title": "Lorem ipsum dolor sit amet.",
+    "description": "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet."
+  },
+  {
+    "id":3,
+    "photo":"aaa",
+    "title": "Lorem ipsum dolor sit amet.",
+    "description": "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet."
+  }
+]
